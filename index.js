f239db185e04c1ac4a3197ff9c7b6d603b999e0c
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2026-05-18 04:05:46 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const cors = require('cors')
-const port = 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT
 
 
 app.use(cors())
 app.use(express.json())
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+    const db = client.db('DocAppointServer')
+    const dataCollection = db.collection('appointmentCollection')
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
